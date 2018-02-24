d2966b8c3697e5f4b3f398ec2b16bd0a69cac8cc
Tiemen <Tiemen2@live.nl>
2018-02-24 14:15:11 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# SceneSource Webhook
+
+Simple Discord webhook that checks for new releases on scnsrc.me
---
+++ index.js
@@ -0,0 +1,39 @@
+const { WebhookClient, RichEmbed } = require("discord.js")
+const fs = require("fs")
+const config = require("./config")
+const data = require("./data.json")
+const webhook = new WebhookClient(config.id, config.token, { disableEveryone: true })
+const RSSFeedEmitter = require("rss-feed-emitter")
+const feed = new RSSFeedEmitter()
+
+console.log("[WEBHOOK] Ready!")
+
+feed.add({ url: "http://feeds.feedburner.com/scnsrc/rss?format=xml", refresh: 60000 }) // every minute
+feed.on("new-item", async item => {
+    console.log("[FEED] New item")
+    if (data.feed.includes(item.guid)) {
+        console.log("[DATA] Exists")
+        return
+    } else {
+        data.feed.push(item.guid)
+        await fs.writeFileSync("./data.json", JSON.stringify(data))
+        console.log("[DATA] Wrote to data.json")
+    }
+    let categories = item.categories.join(" | ")
+    if (categories.includes("Offtopic")) return // filter off-topic out
+    let color = categories.includes("TV") ? "AQUA" : categories.includes("Movies") ? "PURPLE" : "GREY"
+
+    const embed = new RichEmbed()
+        .setAuthor(categories)
+        .setTitle(item.title)
+        .setURL(item.link)
+        .setColor(color)
+        .setTimestamp(item.pubDate)
+
+    console.log("[HOOK] Sending message")
+    webhook.send("New release!", { embeds: [embed] })
+     .then(msg => console.log("[HOOK] Message sent!"))
+     .catch(console.error)
+})
+
+process.on("unhandledRejection", err => console.log(err))
